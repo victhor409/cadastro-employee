1b7ae356a6c05d68e57b21df9774e67546b6d7b5
victhor409 <victhor409@gmail.com>
2019-07-04 16:40:13 -0300

    cadastro employee

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Employee employee = new Employee();
+		
+		System.out.print("Name: ");
+		employee.name=sc.nextLine();
+		System.out.print("Gross Salary: ");
+		employee.grossSalary=sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax=sc.nextDouble();
+		System.out.println();
+		System.out.println(employee);
+		System.out.println();
+		System.out.print("Which percentage to increase salary? ");
+		double percentage=sc.nextDouble();
+		employee.increaseSalary(percentage);
+		System.out.println();
+		System.out.printf("Updated data: "+ employee.name+" , $ "+ employee.increaseSalary(percentage));
+		sc.close();
+
+	}
+
+}
---
+++ src/entites/Employee.java
@@ -0,0 +1,26 @@
+package entites;
+
+public class Employee {
+
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	
+	public double NetSalary(double grossSalary) {
+		return this.grossSalary - tax;
+	}
+	
+	public String toString() {
+		return "Emplyee:"
+		+ name
+		+ ", $"
+		+ String.format( "%.2f",NetSalary(grossSalary));
+	}
+	//metodo para calcular o aumento
+	public double increaseSalary(double percentage) {
+		return percentage=+grossSalary;
+	
+	 }
+	
+}
